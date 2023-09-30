9e9424e406949b5da74829331f7e7d9b2f51bf8c
art-mkhitaryan <artur.mkhitaryan@outlook.com>
2023-09-30 12:51:11 -0400

    change mysq

--- terraform/mysql_vm.tf
+++ terraform/mysql_vm.tf
@@ -5,7 +5,7 @@ resource "google_project_service" "compute" {
 resource "google_compute_instance" "mysql-test" {
   name         = "mysql-test"
   machine_type = "e2-small"
-  zone         = "europe-west3-a"
+  zone         = "europe-west3-b"
 
   boot_disk {
     initialize_params {
