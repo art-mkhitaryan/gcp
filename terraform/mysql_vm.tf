3e37b9c2711cf820aef9174645d54415e911be05
art-mkhitaryan <artur.mkhitaryan@outlook.com>
2023-09-30 10:37:50 -0400

    test#14

--- terraform/mysql_vm.tf
+++ terraform/mysql_vm.tf
@@ -1,30 +1,30 @@
-# resource "google_project_service" "compute" {
-#   service = "compute.googleapis.com"
-# }
+resource "google_project_service" "compute" {
+  service = "compute.googleapis.com"
+}
 
-# resource "google_compute_instance" "mysql-test" {
-#   name         = "mysql-test"
-#   machine_type = "e2-small"
-#   zone         = "europe-west3-a"
+resource "google_compute_instance" "mysql-test" {
+  name         = "mysql-test"
+  machine_type = "e2-small"
+  zone         = "europe-west3-a"
 
-#   boot_disk {
-#     initialize_params {
-#       image = "projects/rocky-linux-cloud/global/images/rocky-linux-9-optimized-gcp-v20230912"
-#       size  = 20
-#       type  = "pd-ssd"
-#     }
-#   }
+  boot_disk {
+    initialize_params {
+      image = "projects/rocky-linux-cloud/global/images/rocky-linux-9-optimized-gcp-v20230912"
+      size  = 20
+      type  = "pd-ssd"
+    }
+  }
 
-#   network_interface {
-#     network = "default"
+  network_interface {
+    network = "default"
 
-#     access_config {
-#       // Ephemeral public IP
-#     }
-#   }
+    access_config {
+      // Ephemeral public IP
+    }
+  }
 
-#   metadata_startup_script = "echo hi > /test.txt"
+  metadata_startup_script = "echo hi > /test.txt"
 
-#   depends_on = [google_project_service.compute]
+  depends_on = [google_project_service.compute]
 
-# }
+}
