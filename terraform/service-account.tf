a1301e4f6abaa3ef0ff30c569f63770ca04b14c4
art-mkhitaryan <artur.mkhitaryan@outlook.com>
2023-10-02 19:43:32 -0400

    Changing structure Oct2

--- terraform/service-account.tf
+++ terraform/service-account.tf
@@ -1,15 +1,15 @@
-resource "google_service_account" "service-a" {
-  account_id = "service-a"
-}
+# resource "google_service_account" "service-a" {
+#   account_id = "service-a"
+# }
 
-resource "google_project_iam_member" "service-a" {
-  project = var.gcp_project
-  role    = "roles/storage.admin"
-  member  = "serviceAccount:${google_service_account.service-a.email}"
-}
+# resource "google_project_iam_member" "service-a" {
+#   project = var.gcp_project
+#   role    = "roles/storage.admin"
+#   member  = "serviceAccount:${google_service_account.service-a.email}"
+# }
 
-resource "google_service_account_iam_member" "service-a" {
-  service_account_id = google_service_account.service-a.id
-  role               = "roles/iam.workloadIdentityUser"
-  member             = "serviceAccount:avian-cosmos-400510.svc.id.goog[staging/service-a]"
-}
+# resource "google_service_account_iam_member" "service-a" {
+#   service_account_id = google_service_account.service-a.id
+#   role               = "roles/iam.workloadIdentityUser"
+#   member             = "serviceAccount:avian-cosmos-400510.svc.id.goog[staging/service-a]"
+# }
